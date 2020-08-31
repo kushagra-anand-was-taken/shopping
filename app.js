e0a7039119d4812ec08ae97742579e52ae6937a2
Kushagra Anand <kushrokz95@gmail.com>
2020-08-31 17:03:04 +0530

    final

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -24,6 +25,13 @@ app.use("/api/products", require("./routes/products"));
 app.use("/api/order", require("./routes/order"));
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
